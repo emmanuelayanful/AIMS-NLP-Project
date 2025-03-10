10aaf4b736024a06b681bfd58288f3a31bf84f81
emmanuelayanful <emmanuelka@aims.ac.za>
2025-03-10 11:24:58 +0200

    some code modifications

--- dataPreprocess.py
+++ dataPreprocess.py
@@ -13,6 +13,7 @@ import jsonlines
 from dataclasses import dataclass, field
 from typing import Optional, List
 import language_map as lm
+from datasets import Dataset
 
 from huggingface_hub import login
 from datasets import load_dataset, Dataset, DatasetDict
@@ -105,6 +106,23 @@ def format_to_comet(examples, direction):
         "translation": [{"src": ex[src], "mt": ex[tgt]} for ex in examples["translation"]]
     }
 
+def get_top_sentences(dataset, model, batch_size, k, device, seed):
+    top_k_heap = []
+    dataset = dataset.shuffle(seed=seed)
+
+    for i in range(0, len(dataset), batch_size):
+        batch = dataset[i : i + batch_size]
+        translations = batch["translation"]
+        scores = list(map(float, model.predict(translations, gpus=1 if device == "cuda" else 0)[0]))
+
+        for translation, score in zip(translations, scores):
+            heapq.heappush(top_k_heap, (score, translation["src"], translation["mt"]))
+            if len(top_k_heap) > k:
+                heapq.heappop(top_k_heap)
+
+    return Dataset.from_list([{"translation": {"src": src, "mt": mt}} for _, src, mt in sorted(top_k_heap, key=lambda x: -x[0])])
+
+
 def convert_json(data, dataset_name, filename, direction, language_pair):
     src, tgt = direction.split("-")
     
@@ -135,7 +153,7 @@ def convert_json(data, dataset_name, filename, direction, language_pair):
         }
 
     if dataset_name == "allenai/wmt22_african":
-        mapped_dataset = data.map(rename_comet).select_columns(["translation"])
+        mapped_dataset = data.map(rename_comet)#.select_columns(["translation"])
     elif dataset_name == "masakhane/mafand":
         mapped_dataset = data.map(rename_mafand)
     else:
@@ -156,14 +174,15 @@ def merge_jsonlines(files, output_file):
 def main():
     parser = HfArgumentParser(ScriptArguments)
     script_args = parser.parse_args_into_dataclasses()[0]
-    if script_args.max_size is not None and script_args.max_size < script_args.top_k_train:
-        script_args.top_k_train = script_args.max_size
-    if script_args.max_size is not None and script_args.max_size < script_args.top_k_val:
-        script_args.top_k_val = script_args.max_size
-    if script_args.max_size is not None and script_args.max_size < script_args.top_k_test:
-        script_args.top_k_test = script_args.max_size
+    
+    if script_args.max_size is None:
+        train_max_size = script_args.top_k_train * 2
+        val_max_size = script_args.top_k_val * 2
+        test_max_size = script_args.top_k_test * 2
     else:
-        script_args.max_size = script_args.top_k_train
+        train_max_size = script_args.max_size
+        val_max_size = script_args.max_size
+        test_max_size = script_args.max_size
     
     logger.info("Logging into huggingface.")
     login(token=script_args.hf_token)
@@ -190,10 +209,22 @@ def main():
                 model_path = download_model(script_args.comet_model)
                 model = load_from_checkpoint(model_path)
                 
-                train = raw_dataset["train"].shuffle(seed=script_args.seed).select(range(script_args.max_size)).map(lambda x: format_to_comet(x, language_pair), batched=True)
-                val = raw_dataset["dev"].shuffle(seed=script_args.seed).select(range(script_args.max_size)).map(lambda x: format_to_comet(x, language_pair), batched=True)
-                test = raw_dataset["test"].shuffle(seed=script_args.seed).select(range(script_args.max_size)).map(lambda x: format_to_comet(x, language_pair), batched=True)
+                train = raw_dataset["train"].shuffle(seed=script_args.seed).select(range(train_max_size)).map(lambda x: format_to_comet(x, language_pair), batched=True)
+                val = raw_dataset["dev"].shuffle(seed=script_args.seed).select(range(val_max_size)).map(lambda x: format_to_comet(x, language_pair), batched=True)
+                test = raw_dataset["test"].shuffle(seed=script_args.seed).select(range(test_max_size)).map(lambda x: format_to_comet(x, language_pair), batched=True)
                 
+
+                logger.info(f"\nSelecting top sentences with {script_args.comet_model} model.")
+                train = get_top_sentences(
+                    train, model, script_args.batch_size, script_args.top_k_train, script_args.device, script_args.seed
+                )
+                val = get_top_sentences(
+                    val, model, script_args.batch_size, script_args.top_k_val, script_args.device, script_args.seed
+                )
+                test = get_top_sentences(
+                    test, model, script_args.batch_size, script_args.top_k_test, script_args.device, script_args.seed
+                )
+            
                 comet_name = script_args.comet_model.split("/")[1]
                 output_path = os.path.join(script_args.output_dir, dataset_name, comet_name, new_language_pair)
                 os.makedirs(output_path, exist_ok=True)
